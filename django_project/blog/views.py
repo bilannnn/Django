29cab19f4d08dbffef7561f46f3f92bda641f804
bilannnn <bilan.r.27.03@gmail.com>
2020-10-14 10:53:54 +0300

    database and migrations

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        'author': 'Rostysav Bilan',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'October 13, 2020'
-    },
-{
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'October 13, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
